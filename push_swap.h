32db6f8a086f930e31d1476684317330ff13219a
Kharbachzoubair <zoubairkharbach@gmail.com>
2025-02-01 12:16:30 +0100

    sure

--- push_swap.h
+++ push_swap.h
@@ -6,29 +6,41 @@
 /*   By: zkharbac <zkharbac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 10:41:55 by zkharbac          #+#    #+#             */
-/*   Updated: 2025/02/01 11:56:13 by zkharbac         ###   ########.fr       */
+/*   Updated: 2025/02/01 12:15:48 by zkharbac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 #define PUSH_SWAP_H
 
-#include <stddef.h>
 #include <limits.h>
+#include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>   
+
+
 typedef struct s_stack
 {
     int value;
     struct s_stack *next;
 } t_stack;
 
-int ft_strlen(char *s);
-int count_words(char *s);
-char *join_arg(int argc, char **argv);
-int validate_numbers(char **numbers);
-char **ft_split(char *s);
-int is_within_int_range(char *str);
-int has_duplicates(char **numbers);
-int ifnumber(char *s);
+// Function prototypes for utility functions
+int     ft_strlen(char *s);
+int     count_words(char *s);
+char   *join_arg(int argc, char **argv);
+int     validate_numbers(char **numbers);
+char  **ft_split(char const *s);
+void    free_split(char **split);
+
+// Prototypes for functions used in validation
+int     is_valid_number(char *str);
+int     is_within_int_range(char *str);
+int     has_duplicates(char **numbers);
+
+// Custom conversion functions
+long    ft_atol(char *str);
+int     ft_atoi(char *str);
+
 #endif
--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: zkharbac <zkharbac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 10:53:45 by zkharbac          #+#    #+#             */
-/*   Updated: 2025/02/01 11:56:18 by zkharbac         ###   ########.fr       */
+/*   Updated: 2025/02/01 12:11:48 by zkharbac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,34 @@ int count_words(char *s)
     }
     return count;
 }
+int	ft_atoi(const *str)
+{
+	int					i;
+	int					sign;
+	unsigned long long	result;
 
+	i = 0;
+	result = 0;
+	sign = 1;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		if (result > 9223372036854775807 && sign == 1)
+			return (-1);
+		if (result > 9223372036854775807 && sign == -1)
+			return (0);
+		i++;
+	}
+	return (result * sign);
+}
 char *join_arg(int argc, char **argv)
 {
     int i = 1, j, k = 0, len = 0;
