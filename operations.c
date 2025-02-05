773202598eacc85deb90e805035db8296a008bb9
Kharbachzoubair <zoubairkharbach@gmail.com>
2025-02-06 00:35:35 +0100

    done

--- operations.c
+++ operations.c
@@ -10,3 +10,60 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
+void swap(t_stack **stack)
+{
+    if(!*stack || !(*stack)->next )
+        return;
+    
+    t_stack *first=(*stack);
+    t_stack *second=(*stack)->next;
+     first->next=second->next;
+     second=first->next;
+     *stack=second;
+
+
+}
+
+void push(t_stack **from,t_stack **to)
+{
+    if(!(*from))
+        return;
+    t_stack *tmp=(*from);
+    (*from)=(*from)->next;
+    tmp->next=(*to);
+    (*to)=tmp;
+    
+    
+}
+void rotate(t_stack **stack)
+{
+    if(!(*stack) || !((*stack)->next))
+        return;
+    t_stack *tmp=(*stack);
+    (*stack)=(*stack)->next;
+    t_stack *last=(*stack);
+    while(last->next)
+    {
+        printf("%d",last->value);
+        last=last->next;
+    }
+    last->next=tmp;
+    tmp->next=NULL;
+
+}
+
+void reverse_rotate(t_stack **stack)
+{
+    if(!(*stack)|| !((*stack)->next))
+        return;
+    t_stack *prev=(*stack);
+    (*stack)=(*stack)->next;
+    t_stack *last=(*stack);
+    while(last->next)
+        last=last->next;
+    
+    last->next=prev;
+    prev->next=NULL;
+}
