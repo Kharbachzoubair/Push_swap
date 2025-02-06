baa20798ee91be9caf811dff5ee9cf80204869d3
Kharbachzoubair <zoubairkharbach@gmail.com>
2025-02-06 01:22:42 +0100

    g

--- operations.c
+++ operations.c
@@ -14,56 +14,126 @@
 
 void swap(t_stack **stack)
 {
-    if(!*stack || !(*stack)->next )
+    if (!*stack || !(*stack)->next)
         return;
     
-    t_stack *first=(*stack);
-    t_stack *second=(*stack)->next;
-     first->next=second->next;
-     second=first->next;
-     *stack=second;
-
-
+    t_stack *first = (*stack);
+    t_stack *second = (*stack)->next;
+    first->next = second->next;
+    second->next = first;
+    *stack = second;
 }
 
-void push(t_stack **from,t_stack **to)
+void sa(t_stack **stack_a)
 {
-    if(!(*from))
-        return;
-    t_stack *tmp=(*from);
-    (*from)=(*from)->next;
-    tmp->next=(*to);
-    (*to)=tmp;
-    
-    
+    swap(stack_a);
+    write(1, "sa\n", 3);
 }
+
+void sb(t_stack **stack_b)
+{
+    swap(stack_b);
+    write(1, "sb\n", 3);
+}
+
+void ss(t_stack **stack_a, t_stack **stack_b)
+{
+    swap(stack_a);
+    swap(stack_b);
+    write(1, "ss\n", 3);
+}
+
+void push(t_stack **from, t_stack **to)
+{
+    if (!(*from))
+        return;
+    t_stack *tmp = (*from);
+    (*from) = (*from)->next;
+    tmp->next = (*to);
+    (*to) = tmp;
+}
+
+void pa(t_stack **stack_a, t_stack **stack_b)
+{
+    push(stack_b, stack_a);
+    write(1, "pa\n", 3);
+}
+
+void pb(t_stack **stack_a, t_stack **stack_b)
+{
+    push(stack_a, stack_b);
+    write(1, "pb\n", 3);
+}
+
 void rotate(t_stack **stack)
 {
-    if(!(*stack) || !((*stack)->next))
+    if (!(*stack) || !((*stack)->next))
         return;
-    t_stack *tmp=(*stack);
-    (*stack)=(*stack)->next;
-    t_stack *last=(*stack);
-    while(last->next)
-    {
-        printf("%d",last->value);
-        last=last->next;
-    }
-    last->next=tmp;
-    tmp->next=NULL;
+    
+    t_stack *tmp = (*stack);
+    (*stack) = (*stack)->next;
+    t_stack *last = (*stack);
+    
+    while (last->next)
+        last = last->next;
+    
+    last->next = tmp;
+    tmp->next = NULL;
+}
 
+void ra(t_stack **stack_a)
+{
+    rotate(stack_a);
+    write(1, "ra\n", 3);
+}
+
+void rb(t_stack **stack_b)
+{
+    rotate(stack_b);
+    write(1, "rb\n", 3);
+}
+
+void rr(t_stack **stack_a, t_stack **stack_b)
+{
+    rotate(stack_a);
+    rotate(stack_b);
+    write(1, "rr\n", 3);
 }
 
 void reverse_rotate(t_stack **stack)
 {
-    if(!(*stack)|| !((*stack)->next))
+    if (!(*stack) || !((*stack)->next))
         return;
-    t_stack *prev=(*stack);
-    (*stack)=(*stack)->next;
-    t_stack *last=(*stack);
-    while(last->next)
-        last=last->next;
     
-    last->next=prev;
-    prev->next=NULL;
+    t_stack *prev = NULL;
+    t_stack *last = (*stack);
+    
+    while (last->next)
+    {
+        prev = last;
+        last = last->next;
+    }
+    
+    prev->next = NULL;
+    last->next = (*stack);
+    (*stack) = last;
+}
+
+void rra(t_stack **stack_a)
+{
+    reverse_rotate(stack_a);
+    write(1, "rra\n", 4);
+}
+
+void rrb(t_stack **stack_b)
+{
+    reverse_rotate(stack_b);
+    write(1, "rrb\n", 4);
+}
+
+void rrr(t_stack **stack_a, t_stack **stack_b)
+{
+    reverse_rotate(stack_a);
+    reverse_rotate(stack_b);
+    write(1, "rrr\n", 4);
 }
--- push_swap.h
+++ push_swap.h
@@ -10,6 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+
 #ifndef PUSH_SWAP_H
 #define PUSH_SWAP_H
 
@@ -19,19 +20,17 @@
 #include <stdio.h>
 #include <unistd.h>  
 
-// Structure declaration
 typedef struct s_stack
 {
     int value;
     struct s_stack *next;
 } t_stack;
 
-
 int     ft_strlen(char *s);
 int     count_words(char *s);
 char   *join_arg(int argc, char **argv);
 int     validate_numbers(char **numbers);
-char  **ft_split(char  *s);
+char  **ft_split(char *s);
 void    free_split(char **split);
 
 int     is_valid_number(char *str);
@@ -39,10 +38,27 @@ int     is_within_int_range(char *str);
 int     has_duplicates(char **numbers);
 int     ifnumber(char *s);
 
-int     ft_atoi( char *str);
+int     ft_atoi(char *str);
 t_stack *create_node(int value);
 t_stack *build_stack(char **numbers);
-void print_stack(t_stack *stack);
-void free_stack(t_stack *stack);
+void    print_stack(t_stack *stack);
+void    free_stack(t_stack *stack);
+
+void    swap(t_stack **stack);
+void    push(t_stack **from, t_stack **to);
+void    rotate(t_stack **stack);
+void    reverse_rotate(t_stack **stack);
+
+void    sa(t_stack **stack_a);
+void    sb(t_stack **stack_b);
+void    ss(t_stack **stack_a, t_stack **stack_b);
+void    pa(t_stack **stack_a, t_stack **stack_b);
+void    pb(t_stack **stack_a, t_stack **stack_b);
+void    ra(t_stack **stack_a);
+void    rb(t_stack **stack_b);
+void    rr(t_stack **stack_a, t_stack **stack_b);
+void    rra(t_stack **stack_a);
+void    rrb(t_stack **stack_b);
+void    rrr(t_stack **stack_a, t_stack **stack_b);
 
 #endif
